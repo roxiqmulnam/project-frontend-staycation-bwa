532bf3093a2fe0e03cb0158b1395db8437d5a4d9
Roxiq Mulna Muhammad <roxiqmulna02@gmail.com>
2022-06-21 20:37:37 +0700

    add Category, Testimoni, and Footer

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -3,6 +3,9 @@ import React, { Component } from "react";
 import Header from "parts/Header";
 import Hero from "parts/Hero";
 import MostPicked from "parts/MostPicked";
+import Categories from "parts/Categories";
+import Testimony from "parts/Testimony";
+import Footer from "parts/Footer";
 
 import landingPage from "json/landingPage.json";
 export default class LandingPage extends Component {
@@ -17,6 +20,9 @@ export default class LandingPage extends Component {
         <Hero refMostPicked={this.refMostPicked} data={landingPage.hero} />
         <MostPicked refMostPicked={this.refMostPicked}
           data={landingPage.mostPicked} />
+        <Categories data={landingPage.categories} />
+        <Testimony data={landingPage.testimonial} />
+        <Footer data={landingPage.testimonial} />
       </>
     );
   }
